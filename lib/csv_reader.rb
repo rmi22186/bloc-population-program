721e4e3ded0eabdd88fd21c535ef0a3c3b276583
Robert Ing <robertming@gmail.com>
2013-10-16 09:12:14 -0700

    update comments

--- lib/csv_reader.rb
+++ lib/csv_reader.rb
@@ -1,24 +1,20 @@
-# Responsible for taking in the csv and passing back each
-# area as a hash
+# BLOC.IO - "Responsible for taking in the csv and passing back each area as a hashes"      # each area is a line, but i'm not seeing area variables here?
 
 class CSVReader
   
-  attr_accessor :fname, :headers
+  attr_accessor :fname, :headers    # make it so "fname" and "headers" variables can be set as instance variables
   
-  def initialize(filename)          # automatically run when program starts
+  def initialize(filename)          # automatically run when program starts, is filename a Ruby default variable that knows to look for a file in the directory?  
     @fname = filename               # create instance variable for filename to be used in other methods
   end
 
   def read                          
     f = File.new(@fname, 'r')       # initiate read only view of file.  when a giant file is being read, where is the temporary data being stored???
-    
-    
     self.headers = f.readline       # Grab the first line, which initially is one long string???  readline = first line?
-
     
     while (!f.eof? && next_line = f.readline)   # I understand the while it is not the end of the file, but where does "&& next_line = readline" come in, that doesn't seem to be a condition???
-      values = next_line.split(',')             # values is now an array, because we are splitting the CSV by comma.  is this called "value" because it's a hash and is the value part of key/value pair???
-      hash = create_hash(values)                # in this hash, 
+      values = next_line.split(',')             # values is now an array, because we are splitting the CSV by comma.  is this called "value" because it's a hash and is the value part of key/value pair, or is it just a variable assigned to it???
+      hash = create_hash(values)                # are hashes not actually being created here, and starting in ln33 below?
       yield(hash)                               # what does this mean again?
     end
   end
@@ -43,7 +39,7 @@ class CSVReader
   end
 end
 
-class String
+class String                                    # REGEX for converting to snake case to Rubify words
   def underscore
     self.gsub(/::/, '/').
     gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
--- lib/setup.rb
+++ lib/setup.rb
@@ -6,14 +6,13 @@ class Setup
 
   attr_accessor :areas
   def initialize
-    csv = CSVReader.new("./zipcode-db.csv")   # calling a new CSVReader class using the csv file and assigning it to variable csv
+    csv = CSVReader.new("./zipcode-db.csv")   # create a new CSVReader class using the csv file and assigning it to variable csv.  Is this where it links to csv_reader def initialize?
 
-    @areas = []                 # creates empty @area instance
-    csv.read do |item|          # calls read method from csv_reader.rb on csv, which is the read only file
+    @areas = []                 # creates empty @area instance to be filled with [?zip code, estimated_population, city, state?] from areas.rb?
+    csv.read do |item|          # calls read method from csv_reader.rb on csv, which is the read only file.  is csv.read full of hashes now?
       @areas << Area.new(item)  # unsure exactly what happens here as read method is processed??? are only zip code, estimated population, city, and state passed into each @area instance?
     end
 
     self                        # why is "self" being returned?  what will self be?
   end
 end
-
